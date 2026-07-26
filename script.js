49cf1ba5440d640da7ac742e71e30d7cd419e99d
biruk <brookzewdu58.com>
2026-07-26 13:41:01 -0700

    finished

--- script.js
+++ script.js
@@ -16,3 +16,19 @@ timeout = setTimeout(mouseStopped, 1000);
 document.addEventListener('mouseout',() =>{
     spotlight.style.display = "none";
 })
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btn = document.querySelector('.button');
+const iconClose = document.querySelector('.icon-close')
+
+registerLink.addEventListener('click', ()=> {
+    wrapper.classList.add('active'); 
+});
+loginLink.addEventListener('click', ()=> {
+    wrapper.classList.remove('active'); 
+}); 
+btn.addEventListener('click', ()=> {
+    wrapper.classList.add('active-popup')});
+iconClose.addEventListener('click', ()=> {
+    wrapper.classList.remove('active-popup')});
